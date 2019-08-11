f19c5b2d94797db0b0702b67204e81c01d445838
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-08-11 13:42:55 -0700

    Add script for running the engine

---
+++ rungame.sh
@@ -0,0 +1,16 @@
+if [ -z $QUAKEDIR ]; then
+	echo "ERROR: Please set QUAKEDIR to your quake install directory"
+	exit
+fi
+
+if [ -f "./bin/linux64/engine" ]; then
+	echo "Running engine..."
+	./bin/linux64/engine -basedir "$QUAKEDIR"
+	exit
+fi
+
+if [ -f "./bin/linux32/engine" ]; then
+	echo "Running engine..."
+	./bin/linux32/engine -basedir "$QUAKEDIR"
+	exit
+fi
