01d35f135c8fae9d84cdcdd4f47ea30b13fffb1e
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-08-11 15:19:36 -0700

    Update QPC scripts and add libq

--- src/_create_quake_projects.bat
+++ src/_create_quake_projects.bat
@@ -1,5 +1,5 @@
 @echo off
 
-py devtools/qpc/qpc.py /basefile "_qpc_scripts/_default.qpc_base" /add vkquake /types vstudio /masterfile vkquake
+py devtools/qpc/qpc.py /basefile "_qpc_scripts/_default.qpc_base" /add vkquake libq /types vstudio /masterfile vkquake
 
 pause
---
+++ src/_create_quake_projects.sh
@@ -0,0 +1,3 @@
+
+python3 ./devtools/qpc/qpc.py /basefile "_qpc_scripts/_default.qpc_base" /add vkquake libq /types vstudio /masterfile vkquake
+ 
